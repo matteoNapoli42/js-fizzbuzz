b4fd3bd462429aa6ae35be19e13dfe36e8a5380c
matteoNapoli42 <matteon4242@gmail.com>
2023-08-25 16:08:00 +0200

    finished

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-for(let i = 0; i<100; i++){
-    if(i%3==0)
+for(let i = 1; i<100; i++){
+    
+    if(i%3==0 && i%5==0)
+        console.log("FizzBuzz");
+    else if(i%3 == 0)
         console.log("Fizz");
-    
-    if(i%5==0)
-        console.log("Buzz")
-    
-    if(i%5==0 && i%3==0)
-        console.log("FizzBuzz")
-    else   
+    else if(i%5 == 0)
+        console.log("Buzz");
+    else
         console.log(i);
 }
+       
